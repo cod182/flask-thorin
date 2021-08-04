684b248db898ada28fd0e342d867915a902ca8e5
Codie Stephens-Evans <codie18@gmail.com>
2021-08-04 11:57:07 +0000

    Deployment to heroku

--- run.py
+++ run.py
@@ -1,12 +1,19 @@
 # Importing the os library
 import os
+import json
 
 # Importing flask class
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+
+if os.path.exists("env.py"):
+    import env
 
 # Creating an instance of the class and sotring in variable app
 # First argument of the Flask class is the name of the apps module - Our package
 app = Flask(__name__)
+
+app.secret_key = os.environ.get("SECRET_KEY")
+#gets the secret key from env.py
 
 # App route decorator. When browsing to home ( / ) the function runs
 @app.route("/")
@@ -16,16 +23,32 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template('about.html', page_title="About", company=data)
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template('member.html', member=member)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template('contact.html')
+    if request.method == "POST":
+        flash("Thanks {}, message received!".format(
+            request.form.get("name")))
+    return render_template('contact.html', page_title="Contact")
 
 @app.route("/careers")
 def careers():
-    return render_template('careers.html')
+    return render_template('careers.html', page_title="Careers")
 # __main__ is the default module in python
 if __name__ == "__main__":
     app.run(
